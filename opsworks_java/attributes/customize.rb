71c026f2b93c986c257ea190af1eaae90df13b59
Archambault <chris_archambault@nj01-6p1g8wn.mhf.mhc>
2015-06-23 07:35:06 -0400

    Adding override for java_opts for tomcat

---
+++ opsworks_java/attributes/customize.rb
@@ -0,0 +1,2 @@
+normal['opsworks_java']['jvm_options'] = '-Xms4096m -Xmx4096m -XX:+UseG1GC -XX:MaxGCPauseMillis=500'
+normal['opsworks_java']['tomcat']['java_opts'] = '-Xms4096m -Xmx4096m -XX:+UseG1GC -XX:MaxGCPauseMillis=500'
