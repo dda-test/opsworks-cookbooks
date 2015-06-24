a1bc443dd30a3dbc695ce9360a4442d8cc87cbe9
Archambault <chris_archambault@nj01-6p1g8wn.mhf.mhc>
2015-06-24 13:03:34 -0400

    cleaned up escaping of sed command

--- dda-kapow/recipes/kapow-run-mc.rb
+++ dda-kapow/recipes/kapow-run-mc.rb
@@ -1,6 +1,6 @@
 execute "update proxy passing to use kapow mgt console port" do
 	environment "PATH" => "/bin:#{ENV["PATH"]}"
-	command "sed -i 's/^\(Proxy.*:\)\(8080\)\(\/\)$/\150080\3/g' /etc/httpd/conf/httpd.conf"
+	command "sed -i 's/^\(Proxy.*:\)\(8080\)\(\/\)$/\\150080\\3/g' /etc/httpd/conf/httpd.conf"
 end
 
 execute "run management console" do
