e2484f87f48ca1377c5daa0ae05067ebab0e73cb
Archambault <chris_archambault@nj01-6p1g8wn.mhf.mhc>
2015-06-24 12:42:55 -0400

    added cwd for working directory

--- dda-kapow/recipes/kapow-setup.rb
+++ dda-kapow/recipes/kapow-setup.rb
@@ -7,11 +7,14 @@ end
 
 execute "download package" do
   environment "PATH" => "/usr/bin:#{ENV["PATH"]}"
+  cwd "#{node[:kapow][:install_dir]}"
   command "wget -q https://s3.amazonaws.com/#{node[:kapow][:s3_bucket]}/#{node[:kapow][:archive_name]}"
   not_if { ::File.exist?("#{node[:kapow][:install_dir]}/#{node[:kapow][:archive_name]}")}
 end
 
 execute "untar package" do
+  environment "PATH" => "/bin:#{ENV["PATH"]}"
+  cwd "#{node[:kapow][:install_dir]}"
   command "tar -zxf #{node[:kapow][:install_dir]}/#{node[:kapow][:archive_name]}"
   not_if { ::File.exist?("#{node[:kapow][:install_dir]}/#{node[:kapow][:base_name]}_#{node[:kapow][:version]}_#{node[:kapow][:architecture]}")}
 end
