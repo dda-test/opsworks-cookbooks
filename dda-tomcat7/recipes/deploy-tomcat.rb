51f8a1e0125c1b0f5c762ea25a5d1566037bb845
Archambault <chris_archambault@nj01-6p1g8wn.mhf.mhc>
2015-06-24 09:02:17 -0400

    Added jvm configuration params to tomcat7.conf

--- dda-tomcat7/recipes/deploy-tomcat.rb
+++ dda-tomcat7/recipes/deploy-tomcat.rb
@@ -2,3 +2,20 @@ package 'install tomcat 7 package' do
   package_name 'tomcat7'
   action :install
 end
+
+# if CATALINA_OPTS not there add it to /etc/tomcat7/tomcat7.conf
+if FileTest.file?("/etc/tomcat7/tomcat7.conf")
+
+  service "tomcat7" do
+    action :stop
+  end
+
+  execute "add catalina opts to tomcat7.conf" do
+    command 'grep -s "CATALINA_OPTS" /etc/tomcat7/tomcat7.conf || sed -i -e "\$aCATALINA_OPTS=\"\${CATALINA_OPTS} -Xms4096m -Xmx4096m -XX:+UseG1GC -XX:MaxGCPauseMillis=500\"" /etc/tomcat7/tomcat7.conf'
+  end
+
+  service "tomcat7" do
+    action :start
+  end
+
+end
