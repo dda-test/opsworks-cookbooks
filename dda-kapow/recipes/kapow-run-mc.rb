1bef6e723e8fb90ac704b49e5d76b83b9b35efe6
Archambault <chris_archambault@nj01-6p1g8wn.mhf.mhc>
2015-06-25 07:46:02 -0400

    added include for apache2::service

--- dda-kapow/recipes/kapow-run-mc.rb
+++ dda-kapow/recipes/kapow-run-mc.rb
@@ -1,3 +1,5 @@
+include_recipe "apache2::service"
+
 execute "update proxy passing to use kapow mgt console port" do
 	environment "PATH" => "/bin:#{ENV["PATH"]}"
 	command "sed -i 's/^\\(Proxy.*:\\)\\(8080\\)\\(\\/\\)$/\\150080\\3/g' /etc/httpd/conf/httpd.conf"
